3ca4d026ca3c3cc7eebe2f170afe8bc615aa55c5
IAvecilla <iavecilla@fi.uba.ar>
2022-05-19 10:50:59 -0300

    Implementation of "Add" trait for finite field element - Create test for the new operation

---
+++ src/finite_field_element.rs
@@ -0,0 +1,38 @@
+use std::ops::Add;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FiniteFieldElement<const P: u128> {
+    pub value: i128,
+}
+
+impl<const P: u128> From<i128> for FiniteFieldElement<P> {
+    fn from(value: i128) -> Self {
+        Self { value }
+    }
+}
+
+impl<const P: u128> Add<FiniteFieldElement<P>> for FiniteFieldElement<P> {
+    type Output = Self;
+
+    fn add(self, other_number: Self) -> Self {
+        Self {
+            value: (self.value + other_number.value).rem_euclid(P as i128),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_two_finite_field_elements() {
+        let first_field_element = FiniteFieldElement::<11>::from(1);
+        let second_field_element = FiniteFieldElement::<11>::from(20);
+
+        assert_eq!(
+            first_field_element + second_field_element,
+            FiniteFieldElement::<11>::from(10)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod finite_field_element;
 fn main() {
     println!("Hello, world!");
 }
